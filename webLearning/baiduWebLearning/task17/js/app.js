9a83af831186b946bee402f002983d926c287b62
newcodero <36941532+newcodero@users.noreply.github.com>
2018-07-07 11:10:27 +0800

    Update app.js

--- webLearning/baiduWebLearning/task17/js/app.js
+++ webLearning/baiduWebLearning/task17/js/app.js
@@ -1,129 +1,118 @@
 // JavaScript Document
 //餐厅类
-function Restaurant(money,seats,staffList){
-	this.money=money;
-	this.seats=seats;
-	this.staffList=staffList;
-};
-//餐厅方法
-Restaurant.prototype.hire=function(staff){
-	if(this.staffList.length===0){this.staffList.push(staff)};
-	for(let i=0;i<this.staffList.length;i++){
-		if((!this.staffList[i].waiter && staff.waiter) || (!this.staffList[i].chief && staff.chief)){
-			this.staffList.push(staff);
+class Restaurant{
+	constructor({cash=0,seats=0,staffList=[]}={}){
+		this.cash=cash;
+		this.seats=seats;
+		this.staffList=staffList;
+	}
+	//单例接口
+	static getInstance(param){
+		if(!this.instance){
+			this.instance=new Restaurant(param);
 		};
-		
-	};	
-};
-Restaurant.prototype.fire=function(staff){
-	if(this.staffList.length===1 && this.staffList[0]==staff ){
-		this.staffList.pop(staff);
-	};
-	for(let i=0;i<this.staffList.length;i++){
-		if(this.staffList[i]==staff){
-			this.staffList.splice(i,1);
-		};
-	};
+		return this.instance;
+	}
+	//招聘
+	hire(newStaff){
+		this.staffList.push(newStaff);
+	}
+	//解聘
+	fire(staff){
+		this.staffList=this.staffList.filter((item)=>{reutrn (item.id !== staff.id)});
+	}
 };
 //职员类
-function Staff(id,name,salary){
-	this.id=id;
-	this.name=name;
-	this.salary=salary;
-};
-//职员方法
-Staff.prototype.oneJob=function(){};
-//服务员类
-function Waiter(id,name,salary){
-	waiters=(function(){ 
-		if(typeof waiter===undefined){				  
-			Staff.call(this,id,name,salary);
+let id=1;
+class Staff{
+	constructor({name='',wages=0}={}){
+		this.id=id++;
+		this.name=name;
+		this.wages=wages;
+	}
+	//单例接口
+	static getInstance(param){
+		if(!this.instance){
+			this.instance=new Staff(param);
 		};
-		return {
-			waiter:'waiter',
-			id:id,
-			name:name,
-			salary:salary
-			};/**/
-	})();
-	return waiters;
-};	
-//服务员类继承职员类
-Waiter.prototype=new Staff();
-//服务员类方法
-Waiter.prototype.oneJob=function(foods){
-	order(foods);
-	return foods;
+		return this.instance;
+	}
+	//完成工作
+	doneWork(){
+		console.log("staff完成工作");
+	}
 };
-Waiter.prototype.server=function(foods){
-	serving(foods);
-	return foods;
-};
-//厨师类，继承职员类
-function Chief(id,name,salary){
-	chiefs=(function(){ 
-		if(typeof chief==undefined){				  
-			Staff.call(this,id,name,salary);
+//服务员类继承自职员
+class Waiter extends Staff{
+	constructor(name,wages){
+		super(name,wages);
+	}
+	//完成工作
+	doneWork(order){
+		//如果参数是个数组，则记录客人点菜，如果参数不是数组则是上菜行为
+		if(order.length !== undefined && typeof order !== "string"){
+			console.log("记录点菜");
+			return true;
+		}else{
+			console.log("进行上菜");
+			return true;
+		}
+	}
+	//单例接口
+	static getInstance(param){
+		if(!this.instance){
+			this.instance=new Waiter(param);
 		};
-		return {
-			chief:'chief',
-			id:id,
-			name:name,
-			salary:salary
-			};
-	})();
-	return chiefs;
+		return this.instance;
+	}
 };
-
-Chief.prototype=new Staff();
-//厨师类方法
-Chief.prototype.oneJob=function(foods){
-	cooking(foods);
-	return foods;
+//厨师类，继承自职员
+class Cook extends Staff{
+	constructor(name,wages){
+		super(name,wages);
+	}
+	//完成工作
+	doneWork(menu){
+		console.log("完成烹饪"+menu[0].name);
+		return true;
+	}
+	//单例接口
+	static getInstance(param){
+		if(!this.instance){
+			this.instance=new Cook(param);
+		}
+		return this.instance;
+	}
 };
-//顾客类
-function Customer(num,seats){
-	this.num=num;
-	this.seats=seats;
-};
-//顾客类方法
-Customer.prototype.order=function(foods){
-	return foods;
-};
-Customer.prototype.eat=function(foods){
-	eat(foods);
+//顾客类、
+class Customer{
+	constructor(){}
+	order(menu){
+		//随便点一道菜
+		const index=Math.round((Object.keys(menu).length-1)*Math.random());
+		return menu[index];
+	}
+	eat(){
+		console.log("吃完了");
+		return true;
+	}
 };
 //菜品类
-function Food(name,firstCost,price){
-	this.name=name;
-	this.firstCost=firstCost;
-	this.price=price;
-};
-function order(foods){
-	console.log('order'+foods);
-};
-function serving(foods){
-	console.log('serving'+foods);
-};
-function cooking(foods){
-	console.log('cooking'+foods);
-};
-function eat(foods){
-	console.log('eating');
-};
-
-//测试
-var ifeRestaurant=new Restaurant(10000,20,[]);
-var newCook=new Chief(1,"Tony",15000);
-var newWaiter=new Waiter(2,"Tom",20000);
-ifeRestaurant.hire(newCook);
-ifeRestaurant.hire(newWaiter);
-var customer1=new Customer(1,1);
-var foods=['food1','food2','food3'];
-var customerOrder=customer1.order(foods);
-
-var order1=newWaiter.oneJob(customerOrder);
-var cook1=newCook.oneJob(order1);
-var serving1=newWaiter.server(cook1);
-customer1.eat(serving1);
-//console.log(ifeRestaurant.staffList);
+class Menu{
+	constructor(list){
+		list.forEach((item,index)=>{
+			this[index]={
+				name:item.name,
+				cost:item.cost,
+				price:item.price
+			}
+		})
+	}
+	//单例接口
+	static getInstance(param){
+		if(!this.instance){
+			this.instance=new Menu(param);
+		};
+		return this.instance;
+	}
+}
